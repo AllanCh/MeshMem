55234bc5c40a0bdc922d6efef28b6d0d6e046ee8
AllanCh <edochacon96@gmail.com>
2016-09-06 22:34:33 -0600

    Pruebas de repositorio

---
+++ Conexion.cpp
@@ -0,0 +1,5 @@
+//
+// Created by allan on 06/09/16.
+//
+
+#include "Conexion.h"
---
+++ Conexion.h
@@ -0,0 +1,14 @@
+//
+// Created by allan on 06/09/16.
+//
+
+#ifndef MESH_MEM_CONEXION_H
+#define MESH_MEM_CONEXION_H
+
+
+class Conexion {
+
+};
+
+
+#endif //MESH_MEM_CONEXION_H
