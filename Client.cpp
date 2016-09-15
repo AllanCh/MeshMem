d1d8fa8663f81c8fb96771107cfbc98f1bd91601
AllanCh <edochacon96@gmail.com>
2016-09-14 19:55:01 -0600

    Clases Manejadoras de Json agregadas

--- Client.cpp
+++ Client.cpp
@@ -14,11 +14,12 @@ void Client::error(const char* msg) {
 }
 
 void Client::iniciar(const string texto) {
-    int sockfd, portno;
+    int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
     char buffer[256];
+    char reading[256];
     /*if (argc < 3) {
         fprintf(stderr,"usage %s hostname port\n", argv[0]);
         exit(0);
@@ -40,18 +41,19 @@ void Client::iniciar(const string texto) {
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
         error("ERROR connecting");
-    //printf("Por favor escriba su mensaje: ");
+    printf("Por favor escriba su mensaje: ");
     //bzero(buffer,256);
-    //fgets(buffer,255,stdin)
-    strcpy(buffer,texto.data());
-    send(sockfd,buffer,strlen(buffer),0);
-   /* if (n < 0)
-        error("ERROR writing to socket");*/
-    bzero(buffer,256);
-    read(sockfd,buffer,255);
-    cout<<"Aqui"<<endl;
+    //fgets(buffer,255,stdin);
+    //strcpy(buffer,texto.data());
+    n=write(sockfd,buffer,strlen(buffer));
+
+    if (n < 0)
+        error("ERROR writing to socket");
+      bzero(reading,256);
+      n=read(sockfd,reading,255);
+    //cout<<"Aqui"<<endl;
     //if (n < 0)
-      //  error("ERROR reading from socket");
-    //printf("%s\n",buffer);
+        //error("ERROR reading from socket");
+    printf("%s\n",reading);
     close(sockfd);
 }
