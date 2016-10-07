540ed245264463fcb4c22ac696e665ed3d51709d
AllanCh <edochacon96@gmail.com>
2016-10-07 03:28:54 -0600

    Eliminacion de la funcion stdin en la conexion de sockets

--- Client.cpp
+++ Client.cpp
@@ -14,6 +14,7 @@ void Client::error(const char* msg) {
 }
 
 string Client::iniciar(string texto) {
+
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
@@ -25,6 +26,7 @@ string Client::iniciar(string texto) {
         fprintf(stderr,"usage %s hostname port\n", argv[0]);
         exit(0);
     }*/
+
     portno = getPort();
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
@@ -39,14 +41,15 @@ string Client::iniciar(string texto) {
     bcopy((char *)server->h_addr,
           (char *)&serv_addr.sin_addr.s_addr,
           server->h_length);
+
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
         error("ERROR connecting");
+
     bzero(buffer,256);
-    fgets(buffer,255,stdin);
     strcpy(buffer,texto.c_str());
+
     buffer[texto.length()]='\n';
-    cout<<"Buffer"<<buffer<<"Hola"<<endl;
     n=send(sockfd,buffer,texto.length()+1,0);
     if (n < 0)
         error("ERROR writing to socket");
@@ -56,9 +59,11 @@ string Client::iniciar(string texto) {
         error("ERROR reading from socket");
     printf("%s\n",buffer);
 
-    close(sockfd);
+
+    //close(sockfd);
+
     shutdown(sockfd, SHUT_RDWR);
-    cout<<"Buffer"<<buffer<<"Hola de nuevo"<<endl;
+
 
     return buffer;
 }
